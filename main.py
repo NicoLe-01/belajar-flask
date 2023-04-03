be6ed0945887506c926160af742cf64c618cd3eb
NicoLe-01 <nicosiahaan9898@gmail.com>
2023-04-03 21:33:43 +0800

    module and feat: add flask, tailwind, flowbite and input image

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello')
+from flask import Flask, request, render_template
+
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
